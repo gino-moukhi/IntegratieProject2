dfa4fb1f19a7e150463db6bf7e4691820135631f
Lander <lander.tuyteleers@gmail.com>
2018-01-31 09:59:17 +0100

    first commit

---
+++ src/main/java/be/kdg/kandoe/KandoeApplication.java
@@ -0,0 +1,12 @@
+package be.kdg.kandoe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KandoeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KandoeApplication.class, args);
+	}
+}
