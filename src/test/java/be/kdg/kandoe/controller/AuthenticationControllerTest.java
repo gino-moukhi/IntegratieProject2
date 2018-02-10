c3cd2917a214e5704b3a67d5a724639eb1bb6003
sven.matthys <sven.matthys@student.kdg.be>
2018-02-10 14:33:21 +0100

    Added JSON Web Tokens (JWT) -All API calls on /api/public/* are open and don't require authentiation -All API calls on /api/private/* or /api/admin/* require authentication -Client will receive a token when logged in and is valid for a fixed amount of time. This time is defined in the application.properties file and should be in seconds. -Client should always send the token it received with each call Updated the securityconfig file Fixed some bugs

--- src/test/java/be/kdg/kandoe/controller/AuthenticationControllerTest.java
+++ src/test/java/be/kdg/kandoe/controller/AuthenticationControllerTest.java
@@ -22,7 +22,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc
 @RunWith(SpringRunner.class)
-public class RegistrationControllerTest {
+public class AuthenticationControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
