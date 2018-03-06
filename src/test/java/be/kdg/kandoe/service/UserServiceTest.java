2442ea15685bf02a47e6a4fe5dc316c0c704c7e2
Lander Tuyteleers <lander.tuyteleers@gmail.com>
2018-03-06 15:37:10 +0100

    remove @JsonIgnore

--- src/test/java/be/kdg/kandoe/service/UserServiceTest.java
+++ src/test/java/be/kdg/kandoe/service/UserServiceTest.java
@@ -34,7 +34,7 @@ import static org.mockito.Mockito.*;
 @SpringBootTest
 public class UserServiceTest {
 
-/*    private UserService userService;
+ /*  private UserService userService;
 
     private UserRepository userRepository;
 
@@ -72,7 +72,7 @@ public class UserServiceTest {
 
 
     }
-
+/*
 
     @Test
     public void getAllUsersTest(){
@@ -154,7 +154,8 @@ public class UserServiceTest {
         User nonExistingUser = userService.findUserById((long) 2);
     }
 
-
+*/
+ /*
     @Test
     public void getUserByUsernameTest(){
         User spongebob = new User("spongebob", "squarepants", "spongebob", "spongebob@hotmail.com",14, 7,1986,"", Gender.Male, null);
@@ -173,7 +174,7 @@ public class UserServiceTest {
         User user = userService.findUserByUsername("aUserNameThatDoesNotExist");
     }
 
-
+/*
     @Test
     public void getUserByEmailTest(){
         User spongebob = new User("spongebob", "squarepants", "spongebob", "spongebob@hotmail.com",14, 7,1986,"", Gender.Male, null);
